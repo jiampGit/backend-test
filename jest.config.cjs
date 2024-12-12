f7d26c5b77e18b76b391b927b4bd2a9f05a36f82
jiampGit <jiamp109@gmail.com>
2024-12-12 00:08:01 -0300

    Modifica jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,10 @@ const config = {
     preset: "ts-jest",
     moduleNameMapper: {
         '^(\\.{1,2}/.*)\\.js$': '$1',
-    }
+    },
+    //Agrego este código
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.test.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
 module.exports = config;
